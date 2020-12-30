2e9cc7b6409d05dfd90a5b103cc30cdbbefff096
Leonardo Estevao <leooestevao@gmail.com>
2020-12-30 20:57:45 -0300

    Created Admin/Categories delete/update, and improved navigation experience

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const Category = require('./category.js');
 const slugify = require('slugify');
 
-// router.get('/admin/categories/new', (req, res) => {
-//     res.send('Categorias');
-// });
+// LIST CATEGORYS
+router.get('/admin/categories', (req, res) => {
+    Category.findAll().then( resCategories => {
+        res.render('./admin/categories/index.ejs', { categories:resCategories })
+    })
 
+})
+
+// CREATE CATEGORY
 router.get('/admin/categories/new', (req, res) => {
     res.render('./admin/categories/new.ejs');
 
@@ -20,17 +25,62 @@ router.post('/admin/categories/save', (req, res) => {
         NAME: categTitle,
         SLUG: slugify(categTitle)
     }).then( () => {
-        res.redirect('/');
+        res.redirect('/admin/categories');
     })
     else
     res.render('./admin/categories/new.ejs');
 })
 
-router.get('/admin/categories', (req, res) => {
-    Category.findAll().then( resCategories => {
-        res.render('./admin/categories/index.ejs', { categories:resCategories })
-    })
 
+// DELETE CATEGORY
+router.post('/admin/categories/delete', (req, res) => {
+    let categId = req.body.categId;
+    if(categId == undefined)
+        // return res.send(categId)
+        return res.send('Erro ao excluir categoria, id inválido!');
+    if(isNaN(categId))
+        return res.send('Erro ao excluir categoria, id passado nao é um número!');
+
+    Category.destroy({
+        where: {
+            ID: categId
+        }
+    }).then( () => {
+        res.redirect('/admin/categories');
+    })
+})
+
+
+// EDIT CATEGORY
+
+router.get('/admin/categories/edit', (req, res) => {
+    let categId = isNaN(req.query['categId']) ? undefined : req.query['categId'];
+
+    Category.findByPk(categId).then( result => {
+        if(categId == undefined)
+            res.redirect('/admin/categories');
+        res.render('./admin/categories/edit.ejs', {
+            result
+        })
+    }).catch(err => {
+        res.redirect('/admin/categories');
+    })
+})
+
+router.post('/admin/categories/update', (req, res) => {
+    let categId = req.body.categId;
+    let categName = req.body.categName;
+    Category.update(
+        {// Updated field
+            NAME: categName,
+            SLUG: slugify(categName)
+        },
+        {
+            where:
+            {ID: categId}
+        }).then(() =>{
+            res.redirect('/admin/categories')
+        })
 })
 
 module.exports = router;
